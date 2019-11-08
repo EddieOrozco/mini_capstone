367089fee4a2c776c927d23b47ddb3576723e72f
EddieOrozco <orozcoe230@gmail.com>
2019-11-07 20:58:58 -0600

    changed to RESTful API

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,16 @@
 class Api::ProductsController < ApplicationController
-  def products_method
+  def index
+    @products = Product.all
+    render "index.json.jb"
+  end
+
+  def first_products_method
     @products = Product.first
-    render "products.json.jb"
+    render "first_product.json.jb"
+  end
+
+  def product_params_method
+    @products = Product.first
+    render "product_params.json.jb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
-    get "/products_url" => "products#products_method"
+    get "/products" => "products#index"
+    get "/first_products_url" => "products#first_products_method"
+    get "/product_params_url/:id" => "products#product_params_method"
   end
 end
